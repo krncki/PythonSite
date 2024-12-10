e9a304b264b42dfe7b600eee733f6433118e4516
eric <erickornacki@gmail.com>
2024-12-10 07:58:47 -0500

    added description below columns

--- main.py
+++ main.py
@@ -2,13 +2,19 @@ import streamlit as sl
 
 sl.set_page_config(layout="wide")
 col1, col2 = sl.columns(2)
+content = """
+Hi, I am Eric. I like to dabble in many things within IT. 
+"""
+content2 = """
+Below you can find some apps that I have built in Python. 
+Feel free to contact!
+"""
 
 with col1:
     sl.image("images/photo.jpg")
 
 with col2:
     sl.title("Eric Kornacki")
-    content = """
-    Hi, I am Eric. I like to dabble in many things within IT. 
-    """
     sl.write(content)
+
+sl.write(content2)
